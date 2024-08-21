44791d93c8096c516469dfc34afe8c5785591a54
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-08-21 22:14:17 +0530

    Home Completed
    Add new Navigation bar for business, and add addrepairshop page

--- app/Components/Shop_Header.tsx
+++ app/Components/Shop_Header.tsx
@@ -1,18 +1,20 @@
 import { View ,Text} from '@ant-design/react-native'
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { StatusBar ,Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-
+const shopdash = require('../../assets/shopdash.png')
 
 const Shop_Header :React.FC = () => {
   return (
   
   <SafeAreaView >
     <View style={{padding:20 ,backgroundColor:'#F96D2B'}}>
-
+    <Image style={{height:20,width:20,marginLeft:15}} source={shopdash} />
+    <Text style={{fontSize:8, color:'#ffffff'}}>Shop Owner</Text>
+    <Text style={{fontSize:8, color:'#ffffff'}}>DashBoard</Text>
     <StatusBar backgroundColor="#F96D2B" barStyle="light-content" />
-        <Text>Hello machn</Text>
+    
       
     </View>
     
@@ -20,6 +22,7 @@ const Shop_Header :React.FC = () => {
   
   )
 }
+
 
 export default Shop_Header
 
