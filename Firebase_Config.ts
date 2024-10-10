ab740195559d8558be7c40494188b3a586b064fb
Anjana Gunarathna <anjanagunarathna01@gmail.com>
2024-10-11 03:21:10 +0530

    update firbase

--- Firebase_Config.ts
+++ Firebase_Config.ts
@@ -5,6 +5,7 @@
 import { initializeApp } from "firebase/app";
 import {initializeAuth,  getReactNativePersistence} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
+import { getStorage } from 'firebase/storage';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { APIKEY
 ,AUTHDOMAIN
@@ -29,6 +30,8 @@ const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
      persistence: getReactNativePersistence(ReactNativeAsyncStorage)
   });
 const FIREBASE_DB = getFirestore(FIREBASE_APP);
+const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
 
 
-export {FIREBASE_APP,FIREBASE_AUTH,FIREBASE_DB};
+
+export {FIREBASE_APP,FIREBASE_AUTH,FIREBASE_DB,FIREBASE_STORAGE};
